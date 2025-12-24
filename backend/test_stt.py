3e9e6c7bf2a11febc981f60fd2923e7d57009ea7
JEELDONGA18 <jeeldonga18@gmail.com>
2025-12-24 12:29:42 +0530

    Frontend and Backend Version 2.o...

--- backend/test_stt.py
+++ backend/test_stt.py
@@ -1,6 +1,46 @@
-from services.elevenlabs_stt import transcribe_audio
+# ELEVENLAB TESTING
+# from dotenv import load_dotenv
+# from pathlib import Path
+# import os
 
-with open("test.wav", "rb") as f:
-    audio = f.read()
+# # FORCE load .env from backend folder
+# env_path = Path(__file__).parent / ".env"
+# load_dotenv(dotenv_path=env_path)
 
-print(transcribe_audio(audio))
+# print("ENV PATH:", env_path)
+# print("ELEVENLABS_API_KEY:", os.getenv("ELEVENLABS_API_KEY"))
+
+# from services.elevenlabs_stt import transcribe_audio
+
+# with open("test.wav", "rb") as f:
+#     audio = f.read()
+
+# print(transcribe_audio(audio))
+
+
+# GEMINI TESTING
+# from google import genai
+
+# client = genai.Client(
+#     api_key="AIzaSyBuZwZleExDDesvAnHnBD-swrTGFGmvrBM"
+# )
+
+# response = client.models.generate_content(
+#     model="gemini-2.5-flash",
+#     contents="Say hello and confirm the API is working"
+# )
+
+# print(response.text)
+
+# TTS TESTING
+from services.elevenlabs_tts import stream_tts
+
+output_file = "tts_test.wav"
+
+text = "Hello. This is Ora speaking. ElevenLabs text to speech is working correctly."
+
+with open(output_file, "wb") as f:
+    for chunk in stream_tts(text):
+        f.write(chunk)
+
+print(f"TTS audio saved as {output_file}")
